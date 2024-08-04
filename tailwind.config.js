8da9650d2c6d3bd4f85eb5dc407f7e73fbbf35fb
paalamugan <paalamugan44@gmail.com>
2024-08-04 22:37:12 +0530

    chore: update tailwind.config.js and tsconfig.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /* eslint-disable global-require */
 /* eslint-disable import/no-extraneous-dependencies */
 import tailwindConfig from '@paalan/react-config/tailwind';
-import type { Config } from 'tailwindcss';
 import { fontFamily } from 'tailwindcss/defaultTheme';
 
+/** @type {import('tailwindcss').Config} */
 const config = {
   presets: [tailwindConfig],
   content: [
@@ -24,6 +24,6 @@ const config = {
       },
     },
   },
-} satisfies Config;
+};
 
 export default config;
