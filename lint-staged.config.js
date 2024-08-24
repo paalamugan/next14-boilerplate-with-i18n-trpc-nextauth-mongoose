10f3d1e2cc04e98f37051093e5cca374021ef5f7
paalamugan <paalamugan44@gmail.com>
2024-08-25 01:19:19 +0530

    chore: Update lint-staged.config.ts

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 const lintStagedConfig = {
   '**/*.ts?(x)': () => 'npm run type-check',
-  '**/*.{js,mjs,ts,tsx}': ['turbo lint:js -- --no-ignore --fix', 'prettier --check --write'],
+  '**/*.{js,mjs,ts,tsx}': ['turbo lint:js -- --fix', 'prettier --check --write'],
   '**/*.{md,mdx}': ['turbo lint:md -- --fix', 'prettier --check --write'],
   '**/*.css': ['turbo lint:css -- --fix', 'prettier --write'],
   '**/*.{json,yaml}': ['prettier --check --write'],
