e834e5128e7544550464228a252e4acd974cd22c
paalamugan <paalamugan44@gmail.com>
2024-08-25 02:05:08 +0530

    chore: Remove unused password field in ForgotPasswordForm component

--- src/app/[locale]/(auth)/forgot-password/_components/ForgotPasswordForm.tsx
+++ src/app/[locale]/(auth)/forgot-password/_components/ForgotPasswordForm.tsx
@@ -26,7 +26,6 @@ export const ForgotPasswordForm: FC = () => {
     resolver: zodResolver(forgotPasswordValidationSchema),
     defaultValues: {
       email: '',
-      password: '',
     },
   });
   const toast = useToast();
